a3534ac0f1f747e54769a30f25a344ed9a64b878
Turjo Toufi <turjotoufi@Turjos-MacBook-Air.local>
2026-09-25 21:24:40 +0600

    details er kaaj

--- src/app/workout/[id]/page.tsx
+++ src/app/workout/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getWorkoutById } from "@/Library/api";
+import Image from "next/image";
 
 
 interface WorkoutPageProps {
@@ -12,8 +13,23 @@ const WorkoutPage = async ({ params }: WorkoutPageProps) => {
 
     return (
         <div>
-            <h1>{workout.name}</h1>
-            <p>{workout.description}</p>
+            <div className="grid grid-cols-2 gap-5 container mx-auto mt-10">
+                <div>
+                    <Image className="border-2 rounded-2xl w-full h-170"
+                    src={workout.image} alt="" width={700} height={200} />
+                </div>
+                <div>
+                    <h2>
+                        {workout.name}
+                    </h2>
+                    <p>
+                        {workout.description}
+                    </p>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
